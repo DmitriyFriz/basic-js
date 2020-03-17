9a4150d6ed03087f13960ea067079f8727e037ae
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-03-17 21:40:00 +0300

    feat: add solution for transform-array hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,37 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) throw Error('Not Array');
+
+  let controlSequences = {
+    '--discard-next' : function(arr, i, transformArray) {
+      if (i < arr.length - 1) i++;
+      return [i, transformArray];
+    },
+
+    '--discard-prev' : function(arr, i, transformArray) {
+      if (i) transformArray.pop();
+      return [i, transformArray]
+    },
+
+    '--double-next' : function(arr, i, transformArray) {
+      if (i < arr.length - 1) transformArray.push(arr[i+1]);
+      return [i, transformArray]
+    },
+
+    '--double-prev' : function(arr, i, transformArray) {
+      if (i) transformArray.push(arr[i-1]);
+      return [i, transformArray]
+    }
+  }
+
+  let transformArray = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] in controlSequences) {
+      [i, transformArray] = controlSequences[arr[i]](arr, i, transformArray);
+      continue;
+    }
+    transformArray.push(arr[i]);
+  }
+
+  return transformArray;
 };
